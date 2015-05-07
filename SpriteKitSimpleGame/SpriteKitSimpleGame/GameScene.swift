4e91dbf7ffc9d706ea1dc1e55a01b0082175ecdb
OrleansKlaus <orleansklaus@gmail.com>
2015-05-07 09:15:24 -0300

    Implementado o jogo

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
@@ -0,0 +1,70 @@
+//
+//  GameScene.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Orleans Klaus on 04/05/15.
+//  Copyright (c) 2015 Orleans Klaus. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+
+    
+    // 1
+    let player = SKSpriteNode(imageNamed: "player")
+    
+    override func didMoveToView(view: SKView) {
+        // 2
+        backgroundColor = SKColor.whiteColor()
+        // 3
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        // 4
+        addChild(player)
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(#min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
+        
+        // Create sprite
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        // Position the monster slightly off-screen along the right edge,
+        // and along a random position along the Y axis as calculated above
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        // Add the monster to the scene
+        addChild(monster)
+        
+        // Determine speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        // Create the actions
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
+    }
+    
+    
+    
+    func didMoveToView(){
+        runAction(SKAction.repeatActionForever(
+                    SKAction.sequence([
+                    SKAction.runBlock(addMonster),
+                    SKAction.waitForDuration(1.0)])
+        ))}
+    
+    
+}
---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GameViewController.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Orleans Klaus on 04/05/15.
+//  Copyright (c) 2015 Orleans Klaus. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
+extension SKNode {
+    class func unarchiveFromFile(file : String) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
