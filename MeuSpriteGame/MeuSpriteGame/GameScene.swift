3b00692b111ade979a300a5a9d9e2ebc198e2670
OrleansKlaus <orleansklaus@gmail.com>
2015-05-07 11:01:18 -0300

    alterando meu mosntro

--- MeuSpriteGame/MeuSpriteGame/GameScene.swift
+++ MeuSpriteGame/MeuSpriteGame/GameScene.swift
@@ -9,37 +9,64 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    // 1 Declarando o jogador
+    let player = SKSpriteNode(imageNamed: "jogador")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        // 2 Definindo a cor da Cena para azul
+        backgroundColor = SKColor.whiteColor()
+        // 3 Posicionando o jogador para 10% verticalmente no centro
+        player.position = CGPoint(x: size.width * 0.05, y: size.height * 0.5)
+        // 4 Adicionando o jogador na Cena
+        addChild(player)
         
-        self.addChild(myLabel)
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+            SKAction.runBlock(addMonster),
+            SKAction.waitForDuration(1.0)])
+                ))
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(#min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        // Create Sprite
+        let monster1 = "arvore"
+        let monster2 = "noel"
+        let monster3 = "palhaco"
+        let monster4 = "presente"
+        
+        for i in 0..<4{
+//            let monster = SKSpriteNode(imageNamed: mosnter&&i)
         }
+        
+        // Determina onde para o Sprite ao longo do eixo Y
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        // Posição do monstro ligeiramente fora da tela ao longo da borda direita,
+        // E ao longo de uma posição aleatória ao longo do eixo Y como calculado acima
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        // Adiciona o monstro na Cena
+        addChild(monster)
+        
+        // Determina a velocidade do monstro
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        // Cria as açoes
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))// direcionar o objeto se mover para fora da tela
+        let actionMoveDone = SKAction.removeFromParent()// remover o monstro da cena quando ela não é mais visível
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))// encadear uma seqüência de ações que são executadas em ordem, uma de cada vez
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+    
+
+    
 }
